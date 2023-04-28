317c54b1fc6f13a5c6a2c42d753ac59fcd28fbb5
Josh <taylorj12@uni.coventry.ac.uk>
2023-04-28 21:19:40 +0100

    database setup just needs slight modification and now onto authentication and inserting user data

--- Helpers/PasswordHelper.cs
+++ Helpers/PasswordHelper.cs
@@ -1,21 +1,32 @@
-
+using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace WeatherApp.Helpers
 {
     public class PasswordHelpers
     {
         private string? _password;
+        private IDictionary<string, string> passwordObject = new Dictionary<string, string>();
 
         public PasswordHelpers(string? password)
         {
             _password = password;
         }
 
+        public byte[] GenerateSalt()
+        {
+            var bytes = new byte[128 / 8];
+            using(var rng = new RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(bytes);
+                return Convert.ToBase64String(bytes);
+            }
+            
+        }
         public string HashPassword(out byte[] salt)
         {
-            string hash;
-            string salt;
-            return struct {hash; salt;};
+            
         }
     }
 }
